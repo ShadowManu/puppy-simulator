815a01131f872f5a17860b554e4abf8704dd5416
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-09-24 05:32:26 -0400

    add basic elements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		rb.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * speed);
+	}
+}
