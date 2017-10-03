16c38325d7f961b7fc34ed33e780bb4d3ee083cc
María Lourdes Garcia Florez <marylo281292@gmail.com>
2017-10-03 09:14:21 -0400

    refactor steering logics

--- Assets/Scripts/PuppyController.cs
+++ Assets/Scripts/PuppyController.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class PuppyController : MonoBehaviour {
   public GameObject player;
-
   public Mode mode = Mode.None;
-
-  public float maxSpeed;
-  public float minDistance;
-  public float maxDistance;
-  public float timeToTarget;
 
   public GameObject target { get { return player; } }
   public float targetDistance { get { return (target.transform.position - transform.position).magnitude; } }
@@ -22,34 +16,16 @@ public class PuppyController : MonoBehaviour {
 
   /* Update is called once per frame */
   void Update() {
-    Steering steering = Steering.GetSteering(gameObject, target, mode, maxSpeed, timeToTarget);
+    Steering steering = Steering.GetSteering(gameObject, target, new SteeringOptions(mode));
+
+    updatePosition(steering);
 
     // Change mode
     if (Input.GetKeyDown("m")) mode = mode.NextMode();
-
-    // Update position
-    if (CanMove()) {
-      switch (mode) {
-        case Mode.Seek:
-        case Mode.SeekNoOvershoot:
-        case Mode.Arriving:
-          transform.position += steering.velocity * Time.deltaTime;
-          break;
-        
-        case Mode.Flee:
-          transform.position -= steering.velocity * Time.deltaTime;
-          break;
-      }
-    }
   }
 
-  /** Defines guard conditions to allow the position to be modified */
-  bool CanMove() {
-    return
-      (mode == Mode.None) ||
-      (mode == Mode.Seek) ||
-      (mode == Mode.SeekNoOvershoot && targetDistance >= minDistance) ||
-      (mode == Mode.Flee && targetDistance <= maxDistance) ||
-      (mode == Mode.Arriving);
+  /** Update the object position given a steerig  */
+  void updatePosition(Steering steering) {
+    transform.position += steering.velocity * Time.deltaTime;
   }
 }
