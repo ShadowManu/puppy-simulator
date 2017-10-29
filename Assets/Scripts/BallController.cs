0ffd7c85616259439ce6bf6e52283da2da37fdda
María Lourdes Garcia Florez <marylo281292@gmail.com>
2017-10-29 15:59:50 -0400

    Throwing Fixed

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallController : MonoBehaviour {
+  public Vector3 gravity;
+  public Vector3 velocity;
+  public GameObject targetPosition;
+  
+  // Use this for initialization
+  void Start () {
+    targetPosition = GameObject.Find("ballTarget1");
+    gravity = new Vector3 (0,-9.8f,0);
+    velocity = Vector3.zero;
+  }
+  
+  // Update is called once per frame
+  void Update () {
 
-    // Use this for initialization
-    void Start () {
-        
+    if (Input.GetKeyDown(KeyCode.Space)) {
+      velocity = Throwing.CalculateThrowing(transform.position, targetPosition.transform.position, 15, gravity);
     }
     
-    // Update is called once per frame
-    void Update () {
-        
-    }
+    updatePosition();
+    if (velocity != Vector3.zero) velocity += gravity * Time.deltaTime;
+    if (transform.position.y <= 0.5) velocity = Vector3.zero;
+
+  }
+
+  void updatePosition() {
+    transform.position += velocity * Time.deltaTime;
+  }
 }
--- Assets/Scripts/Throwing.cs
+++ Assets/Scripts/Throwing.cs
@@ -1,27 +1,21 @@
 using UnityEngine;
 using System.Collections;
 
-public struct Throwing {
-  public Vector3 throwing;
-
-  public Throwing(Vector3 th) {
-    throwing = th;
-  }
+public class Throwing {
   
   public static Vector3 CalculateThrowing(Vector3 start, Vector3 end, float muzzleVel, Vector3 gravity) {
     Vector3 delta;
-    float a,b,c;    
-    float time0,time1,time;
-    float aux;
     Vector3 result;
-    delta = start - end;
-
+    float a,b,c;
+    float aux;
+    float time0,time1,time;
+    delta = end - start;
 
     a = Vector3.Dot(gravity,gravity);
     b = -4 * (Vector3.Dot(gravity,delta) + (muzzleVel*muzzleVel));
     c = 4 * Vector3.Dot(delta,delta);
 
-    if (4*a*c > b*b) { return Vector3.zero; }
+    if (4*a*c > b*b) return Vector3.zero;  
 
     aux = b*b - 4*a*c;
     time0 = Mathf.Sqrt((-b + Mathf.Sqrt(aux)) / (2*a));
@@ -44,7 +38,8 @@ public struct Throwing {
       }
     }
 
-    result = (2*delta - gravity * (time*time)) / (2*muzzleVel*time);
+    result = (2*delta - gravity * (time*time)) / (2*time);
+    Debug.Log("Velocidad: " + result);
     return result;
   }
 }
