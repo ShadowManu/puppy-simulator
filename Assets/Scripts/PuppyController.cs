4e6a34e2e823278d2881e1f8b12c523da81d525b
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-12-04 04:24:34 -0400

    change map path testing

--- Assets/Scripts/PuppyController.cs
+++ Assets/Scripts/PuppyController.cs
@@ -18,29 +18,35 @@ public class PuppyController : MonoBehaviour {
 
   // Path testing
   int index = 0;
-  List<Kinematic> path;
-  Map map;
+  List<Location> path;
 
   void Start() {
     kinematic = new Kinematic(transform);
     steering = new Steering();
     player = GameObject.Find("Player").GetComponent<PlayerController>().kinematic;
 
-    map = Map.GenerateMap();
-    Graph graph = new Graph(map);
+    path = makePathFromMap();
+  }
 
-    Node source = graph.nodes["A"];
-    Node destination = graph.nodes["D"];
+  private List<Location> makePathFromMap() {
+    ShapeCreator creator = GameObject.Find("Map").GetComponent<ShapeCreator>();
+    var mesh = creator.shapes.makeMesh();
+    var graph = new Graph2(mesh);
+    
+    var nodeList = graph.nodes.Values;
+    var src = nodeList.First();
+    var dst = nodeList.Last();
 
-    path = new List<Kinematic>(graph.FindPath(source, destination).Select(node => new Kinematic(node.polygon.center.transform)));
+    var nodePath = graph.FindPath(src, dst);
+    return nodePath.Select(n => new Location(n.position)).ToList();
   }
 
   void Update() {
-    if (Input.GetKeyDown("c")) map.ToggleCenters();
-    if (Input.GetKeyDown("l")) map.ToggleLines();
-    if (Input.GetKeyDown("v")) map.ToggleVertices();
+    // if (Input.GetKeyDown("c")) map.ToggleCenters();
+    // if (Input.GetKeyDown("l")) map.ToggleLines();
+    // if (Input.GetKeyDown("v")) map.ToggleVertices();
 
-    Kinematic target = path[index];
+    Location target = path[index];
 
     if (((target.position - kinematic.position).magnitude) < 1 && index + 1 < path.Count) index++;
 
