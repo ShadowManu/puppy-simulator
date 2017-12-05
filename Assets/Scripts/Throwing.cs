e28fca92fdc90e1bca9b7311797da6b5c17c17b5
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-12-05 17:47:29 -0400

    initial rework of throwing algorithm

--- Assets/Scripts/Throwing.cs
+++ Assets/Scripts/Throwing.cs
@@ -1,45 +1,34 @@
 using UnityEngine;
 using System.Collections;
 
-public class Throwing {
+public class Throwing { 
   
-  public static Vector3 CalculateThrowing(Vector3 start, Vector3 end, float muzzleVel, Vector3 gravity) {
-    Vector3 delta;
-    Vector3 result;
-    float a,b,c;
-    float aux;
-    float time0,time1,time;
-    delta = end - start;
+  public static Vector3 Velocity(Vector3 start, Vector3 end, float velocity, Vector3 gravity) {
+    // Vector from target back to the start
+    Vector3 delta = start - end;
 
-    a = Vector3.Dot(gravity,gravity);
-    b = -4 * (Vector3.Dot(gravity,delta) + (muzzleVel*muzzleVel));
-    c = 4 * Vector3.Dot(delta,delta);
+    // Calculate coeficients of conventional quadratic equation
+    var a = Vector3.Dot(gravity, gravity);
+    var b = -4 * (Vector3.Dot(gravity, delta) + velocity * velocity);
+    var c = 4 * Vector3.Dot(delta, delta);  
 
-    if (4*a*c > b*b) return Vector3.zero;  
+    // Check if there is no real solutions
+    if (4*a*c > b*b) return Vector3.zero;
 
-    aux = b*b - 4*a*c;
-    time0 = Mathf.Sqrt((-b + Mathf.Sqrt(aux)) / (2*a));
-    time1 = Mathf.Sqrt((-b - Mathf.Sqrt(aux)) / (2*a));
+    var root = Mathf.Sqrt(b*b - 4*a*c);
+    var time0 = Mathf.Sqrt((-b + root) / (2*a));
+    var time1 = Mathf.Sqrt((-b - root) / (2*a));
 
-    if (time0 < 0) {
-      if (time1 < 0) { 
-        return Vector3.zero;
-      }
-      else {
-        time = time1;
-      }
-    }
-    else {
-      if (time1 < 0) { 
-        time = time0; 
-      }
-      else {
-        time = Mathf.Min(time0,time1);
-      }
-    }
+    // No positive answers
+    if (time0 < 0 && time1 < 0) return Vector3.zero;
 
-    result = (2*delta - gravity * (time*time)) / (2*time);
-    Debug.Log("Velocidad: " + result);
-    return result;
+    // Choose better time
+    float time = 0;
+    if (time0 > 0 && time1 > 0) time = Mathf.Min(time0, time1);
+    if (time0 > 0 && time0 < time1) time = time0;
+    if (time1 > 0 && time1 < time0) time = time1;
+
+    // Return the firing vector
+    return (2*delta - gravity * (time*time)) / (2*velocity*time);
   }
 }
